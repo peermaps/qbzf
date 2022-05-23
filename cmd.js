5fef6c5f2273c63f42fdadb83af02fcb128d0a4e
substack <substack@bits.coop>
2022-05-22 16:09:54 -1000

    pack/parsing

--- cmd.js
+++ cmd.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var fs = require('fs')
 var opentype = require('opentype.js')
-var commandsToCurves = require('./lib/commands-to-curves.js')
+var pack = require('./lib/pack.js')
 var from = require('from2')
 var pump = require('stream').pipeline
 var varint = require('varint')
@@ -35,64 +35,30 @@ var codes = argv.code
   ? [].concat(argv.code).flatMap(c => c.split(',').map(Number))
   : Object.keys(font.glyphs.glyphs)
 
-isHeader = false
-
+var offset = 0
 pump(from(function (size, next) {
-  if (index >= codes.length) return next(null, null)
   if (isHeader) {
     var header = Buffer.alloc(magic.length + varint.encodingLength(font.unitsPerEm))
     magic.copy(header, 0)
     varint.encode(font.unitsPerEm, header, magic.length)
     isHeader = false
+    offset += header.length
+    console.error('offset=',offset)
     return next(null, header)
   }
+  if (index >= codes.length) return next(null, null)
   while (true) {
     if (index >= codes.length) break
     var g = font.glyphs.glyphs[codes[index++]]
     if (g.unicodes.length === 0) continue
-    return next(null, pack(g))
+    var buf = pack(g)
+    offset += buf.length
+    console.error('offset=',offset)
+    return next(null, buf)
+    //return next(null, pack(g))
   }
   next(null, null)
 }), process.stdout, onerror)
-
-function pack(g) {
-  var curves = commandsToCurves(g.path.commands)
-  var size = varint.encodingLength(curves.length)
-    + varint.encodingLength(g.advanceWidth)
-    + varint.encodingLength(g.leftSideBearing)
-    + varint.encodingLength(g.xMin)
-    + varint.encodingLength(g.yMin)
-    + varint.encodingLength(g.xMax)
-    + varint.encodingLength(g.yMax)
-  for (var i = 0; i < curves.length; i++) {
-    for (var j = 0; j < curves[i].length; j++) {
-      size += varint.encodingLength(curves[i][j])
-    }
-  }
-  var offset = 0
-  var buf = Buffer.alloc(size)
-  varint.encode(curves.length, buf, offset)
-  offset += varint.encode.bytes
-  varint.encode(g.advanceWidth, buf, offset)
-  offset += varint.encode.bytes
-  varint.encode(g.leftSideBearing, buf, offset)
-  offset += varint.encode.bytes
-  varint.encode(g.xMin, buf, offset)
-  offset += varint.encode.bytes
-  varint.encode(g.yMin, buf, offset)
-  offset += varint.encode.bytes
-  varint.encode(g.xMax, buf, offset)
-  offset += varint.encode.bytes
-  varint.encode(g.yMax, buf, offset)
-  offset += varint.encode.bytes
-  for (var i = 0; i < curves.length; i++) {
-    for (var j = 0; j < curves[i].length; j++) {
-      varint.encode(curves[i][j], buf, offset)
-      offset += varint.encode.bytes
-    }
-  }
-  return buf
-}
 
 function usage() {
   console.log(`
--- index.js
+++ index.js
@@ -1,18 +1,74 @@
-var { quadBezierAABB } = require('bezier-intersect')
+var varint = require('varint')
 
-module.exports = function (g) {
-  //g.dx
-  //g.curves
-  //g.grid
-  for (var i = 0; i < g.grid[1]; i++) {
-    var y0 = i/g.grid[1]
-    var y1 = (i+1)/g.grid[1]
-    for (var j = 0; j < g.grid[0]; j++) {
-      var x0 = j/g.grid[0]
-      var x1 = (j+1)/g.grid[0]
-      for (var k = 0; k < g.curves.length; k++) {
-        g.curves[k]
+module.exports = function (src) {
+  var offset = 0
+  for (; offset < src.length && src[offset] !== 0x0a; offset++);
+  //console.error(src.slice(0,offset))
+  offset++
+  var unitsPerEm = varint.decode(src, offset)
+  offset += varint.decode.bytes
+
+  var curves = new Map
+  while (offset < src.length) {
+    var xndigits = varint.decode(src, offset)
+    var ndigits = xndigits>>1
+    offset += varint.decode.bytes
+    var u0 = varint.decode(src, offset)
+    offset += varint.decode.bytes
+    var u1 = -1
+    var key = String(u0)
+    if (xndigits % 2 === 1) {
+      u1 = varint.decode(src, offset)
+      offset += varint.decode.bytes
+      key += ',' + String(u1)
+    }
+    var advanceWidth = varint.decode(src, offset)
+    offset += varint.decode.bytes
+    var leftSideBearing = varint.decode(src, offset)
+    offset += varint.decode.bytes
+    var xmin = varint.decode(src, offset)
+    offset += varint.decode.bytes
+    var ymin = varint.decode(src, offset)
+    offset += varint.decode.bytes
+    var xmax = varint.decode(src, offset)
+    offset += varint.decode.bytes
+    var ymax = varint.decode(src, offset)
+    offset += varint.decode.bytes
+    var px = 0, py = 0
+    var cs = []
+    for (var n = 0; n < ndigits; n++) {
+      var cx = decode(src, offset)
+      offset += varint.decode.bytes
+      var cxr = ((cx%3)+3)%3
+      cx = Math.floor(cx/3)
+      var cy = decode(src, offset)
+      offset += varint.decode.bytes
+      if (cxr === 0) { // M
+        px += cx
+        py += cy
+      } else if (cxr === 1) { // L
+        cs.push([ px, py, cx+px, cy+py ])
+        px += cx
+        py += cy
+      } else if (cxr === 2) { // Q
+        var nx = decode(src, offset)
+        offset += varint.decode.bytes
+        var ny = decode(src, offset)
+        offset += varint.decode.bytes
+        cs.push([ px, py, cx+px, cy+py, nx+px, ny+py ])
+        px += nx
+        py += ny
       }
     }
+    curves.set(key, cs)
   }
+  var result = {
+    data: null,
+    codes: new Map,
+  }
+}
+
+function decode(src, offset) {
+  var x = varint.decode(src, offset)
+  return x % 2 === 1 ? -(x-1)/2-1 : x/2
 }
