b5e81d0401aa1f8d9b70f8d5d25b2822d36c083c
substack <substack@bits.coop>
2022-05-30 21:27:32 -1000

    interval and

--- test/iv.js
+++ test/iv.js
@@ -1,5 +1,6 @@
 var test = require('tape')
 var ivxor = require('../lib/ivxor.js')
+var ivand = require('../lib/ivand.js')
 
 test('interval xor', function (t) {
   var x
@@ -27,5 +28,34 @@ test('interval xor', function (t) {
   x = [0,1,2,3,4]
   t.equal(ivxor(x,5,[100,101,2,3],2,[102,103,104,1,2],3), 7)
   t.deepEqual(x, [0,1,2,3,4,1,3])
+  t.end()
+})
+
+test('interval and', function (t) {
+  var x
+  x = []
+  t.equal(ivand(x,0,[0,1],0,[2,3],0), 0)
+  t.deepEqual(x, [])
+  x = []
+  t.equal(ivand(x,0,[2,3],0,[0,1],0), 0)
+  t.deepEqual(x, [])
+  x = []
+  t.equal(ivand(x,0,[0,2],0,[1,3],0), 2)
+  t.deepEqual(x, [1,2])
+  x = []
+  t.equal(ivand(x,0,[0,3],0,[1,2],0), 2)
+  t.deepEqual(x, [1,2])
+  x = []
+  t.equal(ivand(x,0,[1,2],0,[0,3],0), 2)
+  t.deepEqual(x, [1,2])
+  x = []
+  t.equal(ivand(x,0,[1,2],0,[2,3],0), 0)
+  t.deepEqual(x, [])
+  x = []
+  t.equal(ivand(x,0,[2,3],0,[1,2],0), 0)
+  t.deepEqual(x, [])
+  x = [0,1,2,3,4]
+  t.equal(ivand(x,5,[100,101,2,3],2,[102,103,104,1,2],3), 5)
+  t.deepEqual(x, [0,1,2,3,4])
   t.end()
 })
