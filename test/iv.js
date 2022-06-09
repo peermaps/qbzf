eee037a8eb48adfa1077ac0704169045af6f7ba3
substack <substack@bits.coop>
2022-06-08 15:07:32 -1000

    remove test.only

--- test/iv.js
+++ test/iv.js
@@ -1,7 +1,7 @@
 var test = require('tape')
 var mivxa = require('../lib/mivxa.js')
 
-test.only('multi interval xor+and', function (t) {
+test('multi interval xor+and', function (t) {
   t.deepEqual(mivxa([], [0,1,2,3], [0,1]), [0,1])
   t.deepEqual(mivxa([], [0,1,2,3], [0.5,2.5]), [0.5,1,2,2.5])
   t.deepEqual(mivxa([], [0,1,1,2,1,4], [1,3]), [2,3])
