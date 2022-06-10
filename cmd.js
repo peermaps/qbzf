d0b081be51b2707471765daaa61607d5e8be5c4e
substack <substack@bits.coop>
2022-06-09 14:55:29 -1000

    filter by unicode with -u and index with -x

--- cmd.js
+++ cmd.js
@@ -13,7 +13,8 @@ var argv = minimist(process.argv.slice(2), {
     v: 'version',
     i: 'infile',
     o: 'outfile',
-    c: ['code','codes'],
+    u: ['unicode','unicodes'],
+    x: ['index','indexes'],
     l: 'ls',
   },
   boolean: [ 'help', 'version', 'ls' ],
@@ -28,20 +29,38 @@ if (infile === undefined || argv.help) return usage()
 var data = fs.readFileSync(infile).buffer
 var font = opentype.parse(data)
 
-var index = 0
 var outstream = argv.outfile === '-' ? process.stdout : fs.createWriteStream(argv.outfile)
 var isHeader = true
+var indexes = []
 
-var codes = argv.code
-  ? [].concat(argv.code).flatMap(c => String(c).split(',').map(Number))
-  : Object.keys(font.glyphs.glyphs)
+if (argv.unicodes) {
+  var codes = new Set
+  var ucodes = [].concat(argv.unicodes)
+    .flatMap(c => String(c).split(','))
+  for (var i = 0; i < ucodes.length; i++) {
+    var u = ucodes[i]
+    var m = /^\\?u\+?([0-9A-Fa-f]+)/i.exec(u)
+    codes.add(m ? parseInt(m[1],16) : Number(u))
+  }
+  for (var [index,g] of Object.entries(font.glyphs.glyphs)) {
+    if (codes.has(g.unicode)) {
+      indexes.push(Number(index))
+    }
+  }
+} else if (argv.indexes) {
+  indexes = [].concat(argv.indexes)
+    .flatMap(c => String(c).split(',').map(Number))
+} else {
+  indexes = Object.keys(font.glyphs.glyphs)
+}
 
+var index = 0
 if (argv.ls) {
   return pump(from(function (size, next) {
-    if (index >= codes.length) return next(null, null)
+    if (index >= indexes.length) return next(null, null)
     while (true) {
-      if (index >= codes.length) break
-      var g = font.glyphs.glyphs[codes[index++]]
+      if (index >= indexes.length) break
+      var g = font.glyphs.glyphs[indexes[index++]]
       if (g.unicodes.length === 0) continue
       return next(null, JSON.stringify(g)+'\n')
     }
@@ -57,10 +76,10 @@ pump(from(function (size, next) {
     isHeader = false
     return next(null, header)
   }
-  if (index >= codes.length) return next(null, null)
+  if (index >= indexes.length) return next(null, null)
   while (true) {
-    if (index >= codes.length) break
-    var g = font.glyphs.glyphs[codes[index++]]
+    if (index >= indexes.length) break
+    var g = font.glyphs.glyphs[indexes[index++]]
     if (g.unicodes.length === 0) continue
     return next(null, pack(g))
   }
@@ -74,8 +93,18 @@ function usage() {
       -i INFILE     Read from this font file.
       -o OUTFILE    Write bezier text output to this file. Default: "-" (stdout)
 
+      -l --list     List all codes from the input font file.
+      -u --unicode  Only include glyphs with these unicode values.
+      -x --index    Only include glyphs with these indexes.
+
       -h --help     Show this message.
       -v --version  Print software version (${require('./package.json').version}).
+
+      Unicode and index lists can be specified with multiple flags (-u 97 -u 98 -u 99)
+      or with comma-separated lists (-u 97,98,99).
+
+      Unicodes can be specified in hexadecimal with a leading "u": (-u u0061).
+
   `.trim().replace(/^ {4}/mg,'') + '\n')
 }
 
