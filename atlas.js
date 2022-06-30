592f9799289fa3aefc066b432c20188b8a7d2d8e
substack <substack@bits.coop>
2022-06-29 23:19:26 -1000

    atlas module

---
+++ atlas.js
@@ -0,0 +1,133 @@
+module.exports = Atlas
+
+function Atlas(qbzf, opts) {
+  if (!(this instanceof Atlas)) return new Atlas(qbzf, opts)
+  if (!opts) opts = {}
+  this._qbzf = qbzf
+  this._attributes = opts.attributes || []
+  this._id = new Map
+  this._nextId = 0
+  this._data = {
+    curves: qbzf.curves,
+    grid: {},
+  }
+  this._gridSet = new Set
+  this.grids = []
+}
+
+Atlas.prototype.add = function (opts) {
+  var m = this._qbzf.measure(opts)
+  var h = opts.height || 0.1
+  var ux = h*m.units[0]/this._qbzf.unitsPerEm
+  var uy = h*m.units[1]/this._qbzf.unitsPerEm
+  var g = this._qbzf.write(opts)
+  var d = this._getGrid(g.n)
+  var n = d.positions.length/2
+  var x = opts.offset ? opts.offset[0] : 0, y = opts.offset ? opts.offset[1] : 0
+  d.positions.push(x,y, x+ux,y, x+ux,y-uy, x,y-uy)
+  d.uv.push(0,1, 1,1, 1,0, 0,0)
+  d.cells.push(n+0, n+1, n+2, n+0, n+2, n+3)
+  d.units.push(g.units,g.units,g.units,g.units)
+  d.size.push(g.grid,g.grid,g.grid,g.grid)
+  d.dim.push(g.dimension,g.dimension,g.dimension,g.dimension)
+  var sw = g.strokeWidth
+  d.strokeWidth.push(sw,sw,sw,sw)
+  for (var i = 0; i < this._attributes.length; i++) {
+    var key = this._attributes[i]
+    var v = opts[key]
+    d[key].push(v,v,v,v)
+  }
+  d.grids.push(g)
+  var p = d.grids[d.grids.length-2]
+  g.offset = p ? p.offset + p.grid[0]*p.grid[1]*(p.n*3+2) : 0
+  d.offsets.push(g.offset, g.offset, g.offset, g.offset)
+  var id = opts.id
+  if (id === undefined) {
+    do {
+      id = this._nextId++
+    } while (this._id.has(id))
+  }
+  this._id.set(id, g.n)
+  d.ids.push(id)
+  if (!this._gridSet.has(g.n)) {
+    this._gridSet.add(g.n)
+    this.grids.push(g.n)
+  }
+  return id
+}
+
+Atlas.prototype.remove = function (id) {
+  var n = this._id.get(id)
+  if (n === undefined) return
+  var g = this._data.grid[n]
+  if (!g) return
+  var ix = g.ids.indexOf(id)
+  if (ix >= 0) {
+    g.ids.splice(ix,1)
+    g.grids.splice(ix,1)
+  }
+  if (g.ids.length === 0) {
+    delete this._data.grid[n]
+    this._gridSet.delete(n)
+    var ix = this.grids.indexOf(n)
+    if (ix >= 0) this.grids.splice(ix,1)
+  }
+}
+
+Atlas.prototype._getGrid = function (n) {
+  if (!this._data.grid[n]) {
+    var data = {
+      curves: this._data.curves,
+      positions: [],
+      uv: [],
+      cells: [],
+      offsets: [],
+      units: [],
+      size: [],
+      dim: [],
+      strokeWidth: [],
+      fillColor: [],
+      strokeColor: [],
+      grid: {},
+      grids: [],
+      ids: [],
+    }
+    this._data.grid[n] = data
+    for (var i = 0; i < this._attributes.length; i++) {
+      data[this._attributes[i]] = []
+    }
+    this.grids.push(n)
+    this._gridSet.add(n)
+  }
+  return this._data.grid[n]
+}
+
+Atlas.prototype.build = function () {
+  var ns = Object.keys(this._data.grid)
+  var data = {}
+  for (var i = 0; i < ns.length; i++) {
+    var n = ns[i]
+    var d = this._getGrid(n)
+    Object.assign(d.grid, concat(d.grids))
+    data[n] = d
+  }
+  return data
+}
+
+function concat(grids) {
+  var len = 0
+  for (var i = 0; i < grids.length; i++) {
+    var g = grids[i]
+    len += g.grid[0]*g.grid[1]*(g.n*3+2)
+  }
+  var width = Math.ceil(Math.sqrt(len))
+  var height = Math.ceil(len/width)
+  var data = new Uint8Array(width*height*4)
+  for (var offset = 0, i = 0; i < grids.length; i++) {
+    var g = grids[i], l = g.grid[0]*g.grid[1]*(g.n*3+2)*4
+    for (var j = 0; j < l; j++) {
+      data[offset++] = g.data[j]
+    }
+  }
+  return { width, height, data, dimension: [width,height] }
+}
--- example/atlas.js
+++ example/atlas.js
@@ -1,77 +1,69 @@
 var glsl = require('glslify')
 var regl = require('regl')()
 var QBZF = require('../')
+var Atlas = require('../atlas')
 
-var draw = null
+var draw = null, data = null
 window.addEventListener('resize', frame)
 
-var data = {
-  curves: {},
-  grid: {},
-}
+//fetch('/font/dvs.bzf').then(r => r.arrayBuffer()).then(r => fromData(new Uint8Array(r)))
 fetch('/f/font').then(r => r.arrayBuffer()).then(r => fromData(new Uint8Array(r)))
 
-function addText(qbzf, text, x, y, h, strokeWidth) {
-  var m = qbzf.measure({ text, offset: [x,y] })
-  var ux = h*m.units[0]/qbzf.unitsPerEm
-  var uy = h*m.units[1]/qbzf.unitsPerEm
-  var g = qbzf.write({ text, strokeWidth })
-  if (!data.grid[g.n]) {
-    data.grid[g.n] = {
-      curves: data.curves,
-      positions: [],
-      uv: [],
-      cells: [],
-      offsets: [],
-      units: [],
-      size: [],
-      dim: [],
-      strokeWidth: [],
-      grid: { texture: null },
-      grids: [],
-    }
-  }
-  var d = data.grid[g.n]
-  console.log(g.n,text)
-  var n = d.positions.length/2
-  d.positions.push(x,y, x+ux,y, x+ux,y-uy, x,y-uy)
-  d.uv.push(0,1, 1,1, 1,0, 0,0)
-  d.cells.push(n+0, n+1, n+2, n+0, n+2, n+3)
-  d.units.push(g.units,g.units,g.units,g.units)
-  d.size.push(g.grid,g.grid,g.grid,g.grid)
-  d.dim.push(g.dimension,g.dimension,g.dimension,g.dimension)
-  d.strokeWidth.push(strokeWidth,strokeWidth,strokeWidth,strokeWidth)
-  d.grids.push(g)
-
-  var p = d.grids[d.grids.length-2]
-  g.offset = p ? p.offset + p.grid[0]*p.grid[1]*(p.n*3+2) : 0
-  d.offsets.push(g.offset, g.offset, g.offset, g.offset)
-}
-
 function fromData(buf) {
-  var qbzf = new QBZF(buf)
-  addText(qbzf, 'hello', -0.7, 0.8, 0.2, 40)
-  addText(qbzf, 'ok...', -0.3, 0.2, 0.2, 40)
-  addText(qbzf, 'hmmmmmmm', 0.2, -0.7, 0.1, 80)
-  addText(qbzf, 'what', -0.8, -0.4, 0.15, 50)
-  addText(qbzf, 'cooooool', 0.3, 0.6, 0.1, 100)
-  Object.assign(data.curves, qbzf.curves)
-  data.curves.texture = regl.texture(data.curves)
+  var atlas = new Atlas(new QBZF(buf), {
+    attributes: [ 'fillColor', 'strokeColor' ]
+  })
+  atlas.add({
+    text: 'hello', offset: [-0.9,0.8], height: 0.4,
+    strokeWidth: 10, strokeColor: [0,1,1], fillColor: [0,0,1],
+  })
+  atlas.add({
+    text: 'ok...', offset: [-0.3,0.2], height: 0.2,
+    strokeWidth: 40, strokeColor: [1,1,0], fillColor: [1,0,1],
+  })
+  atlas.add({
+    text: 'hmmmmmmmmmmmmmm', offset: [0.2,-0.7], height: 0.05,
+    strokeWidth: 80, strokeColor: [1,0,0], fillColor: [1,1,0],
+  })
+  atlas.add({
+    text: 'what', offset: [-0.8,-0.4], height: 0.15,
+    strokeWidth: 50, strokeColor: [1,1,1], fillColor: [0.25,0,0.75],
+  })
+  atlas.add({
+    text: 'cooooool', offset: [0.3,0.6], height: 0.1,
+    strokeWidth: 100, strokeColor: [0,1,0], fillColor: [0,0.5,0],
+  })
+  var id = atlas.add({
+    text: 'meow', offset: [-0.2,-0.2], height: 0.3,
+    strokeWidth: 80, strokeColor: [0.9,0.9,0.9], fillColor: [0.2,0.2,0.2],
+  })
+  setTimeout(() => {
+    atlas.remove(id)
+    build(atlas)
+    frame()
+  }, 1000)
 
-  var draws = []
-  var ns = Object.keys(data.grid)
-  for (var i = 0; i < ns.length; i++) {
-    var n = ns[i], d = data.grid[n]
-    Object.assign(d.grid, concat(d.grids))
-    d.grid.texture = regl.texture(d.grid)
-    draws.push(build(n))
-  }
+  build(atlas)
+  var draws = {}
+  atlas.grids.forEach(n => {
+    draws[n] = drawCurves(n)
+  })
   draw = function (data) {
-    for (var i = 0; i < ns.length; i++) {
-      draws[i](data.grid[ns[i]])
+    for (var i = 0; i < atlas.grids.length; i++) {
+      var n = atlas.grids[i]
+      draws[n](data[n])
     }
   }
   frame()
+}
+
+function build (atlas) {
+  data = atlas.build()
+  atlas.grids.forEach(n => {
+    var d = data[n]
+    d.curves.texture = regl.texture(d.curves)
+    d.grid.texture = regl.texture(d.grid)
+  })
 }
 
 function frame() {
@@ -80,7 +72,7 @@ function frame() {
   if (draw) draw(data)
 }
 
-function build(n) {
+function drawCurves(n) {
   return regl({
     frag: glsl`
       precision highp float;
@@ -90,6 +82,7 @@ function build(n) {
 
       varying vec2 vpos, vuv, vunits, vsize;
       varying float vStrokeWidth, voffset;
+      varying vec3 vFillColor, vStrokeColor;
       uniform sampler2D curveTex, gridTex;
       uniform vec2 curveSize, dim;
       uniform float gridN;
@@ -109,9 +102,9 @@ function build(n) {
         float a = 5.0; // aliasing width in font units
         float outline = 1.0-smoothstep(vStrokeWidth-a,vStrokeWidth+a,ldist);
 
-        vec3 fill = vec3(0,0,0);
-        vec3 stroke = vec3(1,1,1);
-        vec3 bg = vec3(0.5,0,1);
+        vec3 fill = vFillColor;
+        vec3 stroke = vStrokeColor;
+        vec3 bg = vec3(0);
 
         vec3 c = mix(
           mix(bg,stroke,outline),
@@ -124,8 +117,10 @@ function build(n) {
     vert: `
       precision highp float;
       attribute vec2 position, uv, units, size;
+      attribute vec3 fillColor, strokeColor;
       attribute float strokeWidth, offset;
       varying vec2 vpos, vuv, vunits, vsize;
+      varying vec3 vFillColor, vStrokeColor;
       varying float vStrokeWidth, voffset;
       void main() {
         vpos = position;
@@ -133,6 +128,8 @@ function build(n) {
         vunits = units;
         vsize = size;
         voffset = offset;
+        vFillColor = fillColor;
+        vStrokeColor = strokeColor;
         vStrokeWidth = strokeWidth;
         gl_Position = vec4(position,0,1);
       }
@@ -150,26 +147,10 @@ function build(n) {
       offset: regl.prop('offsets'),
       units: regl.prop('units'),
       size: regl.prop('size'),
+      fillColor: regl.prop('fillColor'),
       strokeWidth: regl.prop('strokeWidth'),
+      strokeColor: regl.prop('strokeColor'),
     },
     elements: regl.prop('cells'),
   })
-}
-
-function concat(grids) {
-  var len = 0
-  for (var i = 0; i < grids.length; i++) {
-    var g = grids[i]
-    len += g.grid[0]*g.grid[1]*(g.n*3+2)
-  }
-  var width = Math.ceil(Math.sqrt(len))
-  var height = Math.ceil(len/width)
-  var data = new Uint8Array(width*height*4)
-  for (var offset = 0, i = 0; i < grids.length; i++) {
-    var g = grids[i], l = g.grid[0]*g.grid[1]*(g.n*3+2)*4
-    for (var j = 0; j < l; j++) {
-      data[offset++] = g.data[j]
-    }
-  }
-  return { width, height, data, dimension: [width,height] }
 }
